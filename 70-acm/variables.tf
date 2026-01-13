be91ecb3f1b5f8ea4a50046bc56921a3350e0162
siva2j22 <siva2j22@gmail.com>
2026-01-13 11:31:56 +0530

    update

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "dawsjakkam.fun"
+}
+
+variable "zone_id" {
+    default = "Z0462684FV5U34P7ASOD"
+}
---
+++ 80-frontend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z0462684FV5U34P7ASOD"
+}
+
+variable "domain_name" {
+    default = "dawsjakkam.fun"
+}
