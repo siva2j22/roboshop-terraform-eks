0b2220a13e5f61cbc5d76cfe6d0060c4f583a3b5
siva2j22 <siva2j22@gmail.com>
2026-01-21 15:47:06 +0530

    update file

--- 90-eks/main.tf
+++ 90-eks/main.tf
@@ -1,85 +1,98 @@
-resource "aws_lb" "ingress_alb" {
-  name               = "${local.common_name_suffix}-ingress-alb" # roboshop-dev-backend-alb
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [local.ingress_alb_sg_id]
-  # it should be private subnet ids
-  subnets            = local.public_subnet_ids
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0" # this is module version
 
-  enable_deletion_protection = false # prevents accidental deletion from UI
+  name               = local.common_name_suffix
+  # kubernetes_version = "1.33"
+  kubernetes_version = var.eks_version
 
-  tags = merge (
+  addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+    kube-proxy             = {}
+    vpc-cni                = {
+      before_compute = true
+    }
+    metrics-server = {}
+  }
+
+  endpoint_public_access = false
+  enable_cluster_creator_admin_permissions = true
+
+  vpc_id                   = local.vpc_id
+  subnet_ids               = local.private_subnet_ids
+  control_plane_subnet_ids = local.private_subnet_ids
+  create_node_security_group = false
+  create_security_group      = false
+  node_security_group_id = local.eks_node_sg_id
+  security_group_id = local.eks_control_plane_sg_id
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_groups = {
+    blue = {
+      create = var.enable_blue
+      ami_type       = "AL2023_x86_64_STANDARD"
+      kubernetes_version = var.eks_nodegroup_blue_version
+      instance_types = ["m5.xlarge"]
+      iam_role_additional_policies  = {
+        amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
+        amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+      }
+      
+      # cluster nodes autoscaling
+      min_size     = 2
+      max_size     = 10
+      desired_size = 2
+
+      # taints = {
+      #   upgrade = {
+      #     key = "upgrade"
+      #     value = "true"
+      #     effect = "NO_SCHEDULE"
+      #   }
+      # }
+
+      labels = {
+        nodegroup = "blue"
+      }
+    }
+
+    green = {
+      create = var.enable_green
+      ami_type       = "AL2023_x86_64_STANDARD"
+      kubernetes_version = var.eks_nodegroup_green_version
+      instance_types = ["m5.xlarge"]
+      iam_role_additional_policies  = {
+        amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
+        amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+      }
+      
+      # cluster nodes autoscaling
+      min_size     = 2
+      max_size     = 10
+      desired_size = 2
+
+      # taints = {
+      #   upgrade = {
+      #     key = "upgrade"
+      #     value = "true"
+      #     effect = "NO_SCHEDULE"
+      #   }
+      # }
+
+      labels = {
+        nodegroup = "green"
+      }
+    }
+  }
+
+  tags = merge(
     local.common_tags,
     {
-        Name = "${local.common_name_suffix}-ingress-alb"
+        Name = local.common_name_suffix
     }
   )
-}
-
-resource "aws_lb_listener" "ingress_alb" {
-  load_balancer_arn = aws_lb.ingress_alb.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-TLS13-1-3-2021-06"
-  certificate_arn   = local.ingress_alb_certificate_arn
-
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/html"
-      message_body = "<h1>Hi, I am from HTTPS ingress ALB</h1>"
-      status_code  = "200"
-    }
-  }
-}
-
-resource "aws_route53_record" "ingress_alb" {
-  zone_id = var.zone_id
-  name    = "*.${var.domain_name}" # *.daws86s.fun
-  type    = "A"
-  allow_overwrite = true
-
-  alias {
-    # These are ALB details, not our domain details
-    name                   = aws_lb.ingress_alb.dns_name
-    zone_id                = aws_lb.ingress_alb.zone_id
-    evaluate_target_health = true
-  }
-}
-
-resource "aws_lb_target_group" "frontend" {
-  name     = "${local.common_name_suffix}-frontend"
-  port     = 8080
-  protocol = "HTTP"
-  target_type = "ip"
-  vpc_id   = local.vpc_id
-  deregistration_delay = 60 # waiting period before deleting the instance
-
-  health_check {
-    healthy_threshold = 2
-    interval = 10
-    matcher = "200-299"
-    path = "/"
-    port = 8080
-    protocol = "HTTP"
-    timeout = 2
-    unhealthy_threshold = 2
-  }
-}
-
-resource "aws_lb_listener_rule" "frontend" {
-  listener_arn = aws_lb_listener.ingress_alb.arn
-  priority     = 10
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.frontend.arn
-  }
-
-  condition {
-    host_header {
-      values = ["${var.environment}.${var.domain_name}"] # dev.daws86s.fun
-    }
-  }
+  
 }
--- 90-eks/variables.tf
+++ 90-eks/variables.tf
@@ -13,3 +13,23 @@ variable "domain_name" {
 variable "zone_id" {
     default = "Z0462684FV5U34P7ASOD"
 }
+
+variable "eks_version" {
+
+}
+
+variable "eks_nodegroup_blue_version" {
+
+}
+
+variable "eks_nodegroup_green_version" {
+
+}
+
+variable enable_blue {
+
+}
+
+variable enable_green {
+    
+}
